d52002a3328fdec3987bd7d763491f686c7e4387
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-10-29 21:53:16 +0000

    Fix error

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,7 +2,7 @@
 
 let
   scriptFiles = lib.filterAttrs (name: type: type == "regular") (builtins.readDir scriptsDir);
-  scriptDers = lib.mapAttrsToList (name: type: pkgs.writeShellScriptBin name (builtins.readFile (scriptsDir + "/${name}")) scriptFiles);
+  scriptDers = lib.mapAttrsToList (name: type: pkgs.writeShellScriptBin name (builtins.readFile (scriptsDir + "/${name}"))) scriptFiles;
 in
   pkgs.symlinkJoin {
     name = "pscripts";
