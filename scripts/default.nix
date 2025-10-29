5d9c3cc411b2b6d7a4ecd2aec195813142db1d01
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-10-29 21:44:47 +0000

    Fix typo

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, scriptsDir ? ./. }:
 
 let
-  scriptNames = builtins.attrNames (builtins.filterAttrs (name: type: type == "regular") (builtins.readDir scriptsDir));
+  scriptNames = builtins.attrNames (lib.filterAttrs (name: type: type == "regular") (builtins.readDir scriptsDir));
 
   scripts = builtins.listToAttrs (lib.map (name: lib.nameValuePair name (pkgs.writeShellScriptBin name (builtins.readFile (scriptsDir + "/${name}")))) scriptNames);
 
