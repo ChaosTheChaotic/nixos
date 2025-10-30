9e6d77fa1368d34ce8cf29bcec7208e9b03e23c0
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-10-30 01:35:06 +0000

    Quick fix

--- home.nix
+++ home.nix
@@ -164,6 +164,6 @@
         "${pkgs.waybar}/bin/waybar"
       ];
     };
-    home.file.".config/waybar" = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/waybar";
+    home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/waybar";
   };
 }
