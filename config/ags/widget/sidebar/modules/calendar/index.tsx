66fa899a23903b1d1d8b9949b7ae9449d9885fc0
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-02 00:06:45 +0000

    Remove dots

--- config/ags/widget/sidebar/modules/calendar/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Widget } from "astal/gtk4";
-
-export default function CalendarModules(props: Widget.BoxProps) {
-  return <box>Calendar</box>;
-}
--- config/ags/widget/sidebar/types.ts
+++
@@ -1,6 +0,0 @@
-export enum ScreenSide {
-  LEFT = "left",
-  RIGHT = "right",
-  TOP = "top",
-  BOTTOM = "bottom",
-}
