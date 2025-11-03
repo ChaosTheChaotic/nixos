8c40d0b63e2792db951ecd136640fdab1e255457
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-03 20:51:05 +0000

    Add missed dep to env var

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@
       GDK_BACKEND = "wayland";
       LAYER_SHELL_ENABLE = "1";
       GI_TYPELIB_PATH = with pkgs; lib.makeSearchPath "lib/girepository-1.0" [
+	networkmanager
         gtk4
         graphene
         gtksourceview5
