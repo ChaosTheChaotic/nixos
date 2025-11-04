73af8ee0c03e2a2cceaaa6d073a1dbe9bb4caae2
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-04 20:36:38 +0000

    Add new env var

--- home.nix
+++ home.nix
@@ -38,6 +38,7 @@
       	astal.tray
       ];
       GSETTINGS_SCHEMA_DIR = "${pkgs.astal.notifd}/share/gsettings-schemas/astal-notifd-0-unstable-2025-10-23/glib-2.0/schemas";
+      GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules/";
     };
 
     home.sessionPath = [
