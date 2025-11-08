7ed9f72fc36d2cb64c6d7a280a851b7c1f7afbc7
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-08 21:47:46 +0000

    Add new session var

--- home.nix
+++ home.nix
@@ -153,6 +153,7 @@
       };
       sessionVariables = {
         EDITOR = "nvim";
+	MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'";
       };
       syntaxHighlighting.enable = true;
       autosuggestion.enable = true;
