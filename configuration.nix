72dffe2cef04ee1fe82299c850010d01bc7c67fd
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-01-29 13:46:36 +0000

    Update deps

--- configuration.nix
+++ configuration.nix
@@ -217,7 +217,12 @@ in
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
   };
+  
+  security.rtkit.enable = true;
 
   services.flatpak.enable = true;
 
