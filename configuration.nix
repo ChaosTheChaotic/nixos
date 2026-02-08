d53d2286b6929fe21d250381afc2cde5c82a82bc
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-02-08 14:45:07 +0000

    Add kernel patch thats meant to fix the build failing

--- configuration.nix
+++ configuration.nix
@@ -138,6 +138,30 @@ in
     "zswap.max_pool_percent=25"
   ];
 
+  boot.kernelPatches = [
+    {
+      name = "fix-asahi-build";
+      patch = pkgs.writeText "fix-asahi-build.diff" ''
+        	        diff --git a/rust/kernel/iio/common/mod.rs b/rust/kernel/iio/common/mod.rs
+        	        index 570644ce0938..b789e9bf44c9 100644
+        	        --- a/rust/kernel/iio/common/mod.rs
+        	        +++ b/rust/kernel/iio/common/mod.rs
+        	        @@ -2,10 +2,5 @@
+        	         
+        	         //! IIO common modules
+        	         
+        	        -#[cfg(any(
+        	        -    CONFIG_IIO_AOP_SENSOR_LAS = "y",
+        	        -    CONFIG_IIO_AOP_SENSOR_ALS = "m",
+        	        -    CONFIG_IIO_AOP_SENSOR_LAS = "y",
+        	        -    CONFIG_IIO_AOP_SENSOR_ALS = "m",
+        	        -))]
+        	        +#[cfg(any(CONFIG_IIO_AOP_SENSOR_LAS, CONFIG_IIO_AOP_SENSOR_ALS,))]
+        	         pub mod aop_sensors;
+        	      '';
+    }
+  ];
+
   systemd.settings.Manager = {
     DefaultTimeoutStartSec = "15s";
   };
