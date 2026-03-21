0deda15b7a50846663f17309841b0d76555bb2d6
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-03-21 00:45:54 +0000

    Update the shell.nix to work with vi

--- users/chaos/shell.nix
+++ users/chaos/shell.nix
@@ -95,7 +95,7 @@
         man() {
           MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g;s/.\\x08//g\" | bat -p -lman'" command man "$@"
         }
-	eval "$(clogite init)"
+	zvm_after_init_commands+=('eval "$(clogite init)"')
         fastfetch
       '';
     };
