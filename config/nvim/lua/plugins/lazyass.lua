7c165e2d1bc49d6bd5ed448dbd69a7a26ae16a84
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-04-17 14:05:42 +0100

    Update deps and add more things to blink.cmp

--- config/nvim/lua/plugins/lazyass.lua
+++ config/nvim/lua/plugins/lazyass.lua
@@ -258,6 +258,11 @@ return {
 	},
 
 	{
+    "xzbdmw/colorful-menu.nvim",
+		config = function() end
+	},
+
+	{
 		"saghen/blink.cmp",
 		lazy = false,
 		version = "v1.*",
@@ -277,6 +282,29 @@ return {
 				default = { "lsp", "path", "snippets", "buffer" },
 			},
 			signature = { enabled = true },
+			completion = {
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 10,
+				},
+				menu = {
+					draw = {
+						-- We don't need label_description now because label and label_description are already
+						-- combined together in label by colorful-menu.nvim.
+						columns = { { "kind_icon" }, { "label", gap = 1 } },
+						components = {
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									return require("colorful-menu").blink_components_highlight(ctx)
+								end,
+							},
+						},
+					},
+				},
+			},
 		},
 	},
 
