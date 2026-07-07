1284a6dded04cc1672be44b1767babecee620315
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-07-07 21:16:19 +0100

    Update deps

--- flake.nix
+++ flake.nix
@@ -62,31 +62,31 @@
               }
             ];
           };
+        "Nixpad" =
+          let
+            csys = "x86_64-linux";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = csys;
+            specialArgs = { inherit inputs; };
+            modules = [
+              ./hosts/Nixpad/default.nix
+              agenix.nixosModules.default
+              {
+                environment.systemPackages = [ agenix.packages.${csys}.default ];
+              }
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  cpuArch = "skylake";
+                };
+                home-manager.users.chaos = import ./users/chaos/home.nix;
+              }
+            ];
+          };
       };
-      "Nixpad" =
-        let
-          csys = "x86_64-linux";
-        in
-        nixpkgs.lib.nixosSystem {
-          system = csys;
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/Nixpad/default.nix
-            agenix.nixosModules.default
-            {
-              environment.systemPackages = [ agenix.packages.${csys}.default ];
-            }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                cpuArch = "skylake";
-              };
-              home-manager.users.chaos = import ./users/chaos/home.nix;
-            }
-          ];
-        };
     };
 }
